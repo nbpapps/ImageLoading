6d87844ffb442fd09a5f51badb8b28e6ab5e8e8d
niv ben-porath <nbpapps@gmail.com>
2020-05-25 16:16:35 +0300

    Added an SPM

---
+++ Sources/ImageLoading/ImageLoading.swift
@@ -0,0 +1,3 @@
+struct ImageLoading {
+    var text = "Hello, World!"
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import ImageLoadingTests
+
+var tests = [XCTestCaseEntry]()
+tests += ImageLoadingTests.allTests()
+XCTMain(tests)
